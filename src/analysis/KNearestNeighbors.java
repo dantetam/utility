e3bc500456059afd14bf4f56158b01ebdfb6084d
dantetam <dante.tam1@gmail.com>
2016-05-18 21:44:10 -0700

    Prepare to write KNN

---
+++ src/analysis/KNearestNeighbors.java
@@ -0,0 +1,15 @@
+package analysis;
+
+import java.util.List;
+
+import data.Point;
+
+public class KNearestNeighbors {
+
+	public List<Point> data;
+	
+	public KNearestNeighbors(List<Point> data) {
+		this.data = data;
+	}
+	
+}
