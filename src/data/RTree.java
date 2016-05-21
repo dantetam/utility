cf04f6229e844c3a2b7161b952ae45b83527fe17
dantetam <dante.tam1@gmail.com>
2016-05-20 23:59:00 -0700

    R-tree traversal/recursive insert

--- src/data/RTree.java
+++ src/data/RTree.java
@@ -22,14 +22,24 @@ public class RTree<T extends Point> {
 	 * Not leaf, box contains, no children contain - Insert new box child, split if required,
 	 * insert into new box;
 	 * Not leaf, box contains, a child contains - Insert into the child;
-	 * Leaf, contains - Insert into child directly;
-	 * Otherwise, do nothing. 
+	 * Leaf - Insert into leaf directly.
 	 * 
 	 * @param box - the box we're currently looking at
 	 * @param item - the item to be inserted
 	 */
 	private void insert(RTreeBox box, T item) {
-		
+		if (box.children.isEmpty()) {
+			box.insertMaybeSplit(item);
+		} else {
+			for (RTreeBox c: box.children) {
+				//RTreeBox child = (RTreeBox) c;
+				if (c.contains(item)) {
+					insert(c, item);
+					return;
+				}
+			}
+			box.insertMaybeSplitNew(item);
+		}
 	}
 	
 }
